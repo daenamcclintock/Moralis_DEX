c35b79291c78f1a3d90de305db23ad6c4ac25e8a
daenamcclintock <daena.mcclintock@gmail.com>
2022-07-08 14:15:48 -0400

    boilerplate code added

---
+++ main.js
@@ -0,0 +1,25 @@
+/** Connect to Moralis server */
+const serverUrl = "";
+const appId = "";
+Moralis.start({ serverUrl, appId });
+
+async function login() {
+  let user = Moralis.User.current();
+  if (!user) {
+   try {
+      user = await Moralis.authenticate({ signingMessage: "Hello World!" })
+      console.log(user)
+      console.log(user.get('ethAddress'))
+   } catch(error) {
+     console.log(error)
+   }
+  }
+}
+
+async function logOut() {
+  await Moralis.User.logOut();
+  console.log("logged out");
+}
+
+document.getElementById("btn-login").onclick = login;
+document.getElementById("btn-logout").onclick = logOut;
